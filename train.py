21f0cec90e096d998b2bb89e64b792212c39c346
cjh1212 <45911291+cjh1212@users.noreply.github.com>
2022-07-18 16:51:14 +0900

    Create train.py

---
+++ train.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+
+final = pd.read_csv("final.csv")
+
+label = 's5_final_cat'
+
+X = final[['race_African American', 'race_Hispanic', 'race_Caucasian', 'choice_number', 'play_time',
+                'priority_final', 'refusal_final']]
+# X = final_copy.drop(columns=['s5_final_cat', 's5_mean_cat', 's5_final', 's5_mean'])
+y = final[[f'{label}']]
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=final['s5_final_cat'])
+
+
+
+svc = SVC()
+svc.fit(x_train, y_train)
+
+y_pred = svc.predict(x_test)
+
+print(accuracy_score(y_test, y_pred))
