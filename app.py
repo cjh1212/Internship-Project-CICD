f52841e6dd399315593047b19fad0e51739f5593
cjh1212 <45911291+cjh1212@users.noreply.github.com>
2022-07-28 16:13:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def result():
 
     
     if request.method == 'POST':
-        tokenizer = AutoTokenizer.from_pretrained('t/')
+        tokenizer = AutoTokenizer.from_pretrained('tokenization/')
         model = AutoModelForSequenceClassification.from_pretrained("model/")
 
         def tokenize_function(example):
