49038f6d9700bccfccc9c1fd5a1f081f253abfb8
cjh1212 <45911291+cjh1212@users.noreply.github.com>
2022-07-28 12:50:27 +0900

    Update dynamic_test.py

--- dynamic_test.py
+++ dynamic_test.py
@@ -4,7 +4,7 @@ def response_test():
     response = requests.post(
         'http://34.64.254.235:5001/result',
         data = {
-            'sentnce': 'ddd dd efqwecd dfed'
+            'sentence': 'ddd dd efqwecd dfedd'
         }
     )
 
